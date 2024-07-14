1fca5f3404850781cc968e59d56d40f08ea51f72
Alpha-Oumar BARRY <barryalphaoumar597@gmail.com>
2024-07-14 19:35:14 +0200

    fix

--- projet_predateur_proie/src/main/scala/app/beans/Grid.scala
+++ projet_predateur_proie/src/main/scala/app/beans/Grid.scala
@@ -1,6 +1,6 @@
 package app.beans
 
-case class Grid(width: Int, height: Int) {
+case class Grid(val width: Int, val height: Int) {
 
   def isValidCoordinate(coord: Coordinate): Boolean = {
     coord.x >= 0 && coord.x < width && coord.y >= 0 && coord.y < height
